97ad95182c67fb3b6220279ad8201c472516f342
ravim95 <ravim.95@gmail.com>
2020-08-13 17:06:48 +0530

    Update server.spec.ts

--- test/server.spec.ts
+++ test/server.spec.ts
@@ -1,1 +1,10 @@
 
+import {ApiServer} from '../src/server';
+import {Application} from 'express';
+import * as http from 'http';
+import Mock = jest.Mock;
+
+describe('server', () => {
+  test('canary verifies test infrastructure', () => {
+      expect(true).toEqual(true);
+  });
